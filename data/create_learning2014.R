f13f4af48fac2a6402a635c1729913298585f06b
Pedusal <42636774+Pedusal@users.noreply.github.com>
2018-11-06 22:29:05 +0200

    data created

---
+++ data/create_learning2014.R
@@ -0,0 +1,40 @@
+#Petteri Salo
+#6.11.2018
+#Some learning data wrangling
+
+#2
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+str(lrn14) #The data contains 183 observations of 60 variables. 
+dim(lrn14) #and it means that there is 183 rows and 60 colums
+
+#3
+library(dplyr)
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+learning2014 <- subset(learning2014, Points > 0)
+
+setwd("C:/Users/Pedu/Documents/GitHub/IODS-project")
+write.csv(learning2014, file = "data/learning2014.csv")
+
+test <- read.csv("data/learning2014.csv")
+str(test)
