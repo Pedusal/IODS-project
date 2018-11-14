81aee0a3a297719b9ba75a6ade07b56d4ad8a75c
Pedusal <42636774+Pedusal@users.noreply.github.com>
2018-11-14 13:31:02 +0200

    Update create_alc.R

--- data/create_alc.R
+++ data/create_alc.R
@@ -3,12 +3,25 @@
 ##################### add some comment ##########################
 #########################################
 
-#1
-math <- read.table("data/student-mat.csv", sep="\t", header=TRUE
-por <- read.table("data/student-por.csv", sep="\t", header=TRUE
+#3
+math <- read.table("data/student-mat.csv", sep="\t", header=TRUE)
+por <- read.table("data/student-por.csv", sep="\t", header=TRUE)
                
 str(math) #The data contains  observations of  variables. 
 dim(math) #and it means that there is  rows and  colums
                
 str(por) #The data contains 183 observations of 60 variables. 
 dim(por) #and it means that there is 183 rows and 60 colums
+
+#4
+# common columns to use as identifiers
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+# join the two datasets by the selected identifiers
+math_por <- inner_join(math, por, by = join_by, suffix = c(".math", ".por"))
+
+
+
+#5
+
+
