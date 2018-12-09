d06166564d35dea90dbaee2a70f8c79c66abfa2e
Pedusal <42636774+Pedusal@users.noreply.github.com>
2018-12-09 10:00:11 +0200

    Create meet_and_repeat.R

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,51 @@
+
+# 1
+# Read the BPRS data
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep  =" ", header = T)
+
+# Look at the (column) names of BPRS
+names(BPRS)
+
+# Look at the structure of BPRS
+str(BPRS)
+
+# Print out summaries of the variables
+summary(BPRS)
+
+# brief psychiatric rating scale (BPRS)
+# 
+# The BPRS assesses the level of 18 symptom constructs such as hostility, 
+# suspiciousness, hallucinations and grandiosity; each of these is rated 
+# from one (not present) to seven (extremely severe). The scale 
+# is used to evaluate patients suspected of having schizophrenia.
+# Week 0 = treatment week 
+
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = TRUE, sep = '\t')
+
+# Look at the (column) names of BPRS
+names(RATS)
+
+# Look at the structure of BPRS
+str(RATS)
+
+# Print out summaries of the variables
+summary(RATS)
+
+# Nutrition study conducted in three groups of rats. The groups were 
+# put on different diets, and each animal's body weight (grams) was 
+# recorded repeatedly (approximately) weekly, except in week seven 
+# when two recordings were taken) over a 9-week period.
+
+# 2
+
+# Factor treatment & subject
+BPRS$treatment <- factor(BPRS$treatment)
+BPRS$subject <- factor(BPRS$subject)
+
+# 3
+
+# Convert to long form
+BPRSL <-  BPRS %>% gather(key = weeks, value = bprs, -treatment, -subject)
+
+# Extract the week number
+BPRSL <-  BPRSL %>% mutate(week = as.integer(substr(BPRSL$weeks,5,6)))
